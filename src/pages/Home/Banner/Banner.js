23baf75ea0b81dd0e430824e9974e1171950fe3b
rijwan sakib <71920497+rijwansakib@users.noreply.github.com>
2023-03-07 15:25:41 +0600

    'navbar'

---
+++ src/pages/Home/Banner/Banner.js
@@ -0,0 +1,28 @@
+import React from "react";
+import bannerImage from "../../../assets/images/chair.png"
+const Banner = () => {
+  return (
+    <div>
+      <div className="hero bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img
+            src={bannerImage}
+            className="max-w-sm rounded-lg shadow-2xl"
+            alt="banner"
+          />
+          <div>
+            <h1 className="text-5xl font-bold">Box Office News!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
